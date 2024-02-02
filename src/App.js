dd9b5b87e15d7ea3985562a649b3b01c56f51c3e
jaellomiya <jaellomiya@gmail.com>
2024-02-02 11:46:06 +0200

    Set op node.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App"> Weather App
+    <div className="App"> <h1>Weather App</h1>
+    <footer>This project was coded by <a href=""> Jeal Lomiya</a> and is 
+  <a href="https://github.com/jaellomiya/weather-app-react" target="_blank"> open-sourced on GitHub</a></footer>
     </div>
   );
 }
